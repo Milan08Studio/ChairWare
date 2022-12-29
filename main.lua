d0a50aeecf82b9bab92888470ae17e349a49d554
not milan <68651440+Milan08Studio@users.noreply.github.com>
2022-12-29 15:52:46 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,11 @@
 local cringediscord = 'wxThQvKG'
 local text = Instance.new("TextLabel",Instance.new("ScreenGui",game.CoreGui))
-text.Text = "ChairWare is currently broken due to the latest game update!\nI'll be fixing it very soon!\n- !Milan#4563"
+text.Text = "ChairWare is currently broken due to a game update!\nWe're updating very soon, join the discord in the meantime!\ndiscord.gg/"..cringediscord
 text.Position = UDim2.new(0,0,0,0)
 text.Size = UDim2.new(1,1,1,1)
 text.BackgroundTransparency = 1
 text.TextColor3 = Color3.new(1.0, 1.0, 1.0)
-text.TextSize = 30
+text.TextSize = 20
 if syn then
     syn.request({
     Url = "http://127.0.0.1:6463/rpc?v=1",
