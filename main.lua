03754635319f1a1f40331c797fe66856f9c69ca5
not milan <68651440+Milan08Studio@users.noreply.github.com>
2023-01-03 11:34:11 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -183,7 +183,7 @@ game:GetService("RunService").RenderStepped:Connect(function()
     if map then
         for _,v in pairs(map.LootSpawns:GetChildren()) do -- Render loot esp
             local lootInfo = databases.Get("Loot")[v:GetAttribute("Loot")]
-            local lootColor = consts.RARITY_COLOR[consts.RARITY_MAP[lootInfo.Rarity]]
+            local lootColor = consts.RARITY_COLOR[consts.RARITY_MAP[lootInfo.Rarity]] or Color3.new(255,255,255)
             local billboard = v.Model.Border:FindFirstChild("LootEssPee")
             if not config.lootEsp and billboard then billboard:Destroy() end
             if not billboard and config.lootEsp then
