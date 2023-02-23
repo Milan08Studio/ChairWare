746dbd5e8e28c3c8d6786af1e5df8f4bc433e9b7
not milan <68651440+Milan08Studio@users.noreply.github.com>
2023-02-23 14:35:48 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
 local queueonteleport = syn and syn.queue_on_teleport or queue_on_teleport
 if queueonteleport then queueonteleport(game:HttpGet("https://raw.githubusercontent.com/Milan08Studio/ChairWare/main/main.lua")) end
 local games = {
-    [4580204640] = "SurviveTheKiller",
-    [7863262550] = "FOBLOX"
+    [1489026993] = "SurviveTheKiller",
+    [3042846352] = "FOBLOX"
 }
-local currentGame = games[game.PlaceId]
+local currentGame = games[game.GameId]
 if currentGame then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Milan08Studio/ChairWare/main/Games/"..currentGame..".lua"))()
 end
