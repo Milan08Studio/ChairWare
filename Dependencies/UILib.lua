cd71201dfc764d5a5c16b7b7d0f527ebecd4ed3c
not milan <68651440+Milan08Studio@users.noreply.github.com>
2023-03-11 15:01:38 +0100

    Update UILib.lua

--- Dependencies/UILib.lua
+++ Dependencies/UILib.lua
@@ -655,8 +655,8 @@ function lib:Window(text, preset)
                 )
                 toggled = not toggled
             end
-
             Tab.CanvasSize = UDim2.new(0, 0, 0, TabLayout.AbsoluteContentSize.Y)
+            callback(default)
         end
         function tabcontent:Slider(text, min, max, start, callback)
             local dragging = false
