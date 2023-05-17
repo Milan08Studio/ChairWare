a44acb786b07802a361610d9e1e2179b4baba893
not milan <68651440+Milan08Studio@users.noreply.github.com>
2023-05-17 14:12:13 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,16 @@
-local queueonteleport = syn and syn.queue_on_teleport or queue_on_teleport
-if queueonteleport then queueonteleport(game:HttpGet("https://raw.githubusercontent.com/Milan08Studio/ChairWare/main/main.lua")) end
+--[[
+    DO NOT COPY THIS CODE AND EXECUTE IT!!!
+    IT WILL NOT WORK IF I ADD NEW GAMES!
+
+    Instead, please go to https://pastebin.com/raw/ae7XP30j and use the code there!
+
+    Please read this, ESPECIALLY if you are the owner of a script website, and is about to post this script on your site.
+
+    Thanks.
+]]
 local games = {
     [1489026993] = "SurviveTheKiller",
     [3042846352] = "FOBLOX"
 }
-local currentGame = games[game.GameId]
-if currentGame then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Milan08Studio/ChairWare/main/Games/"..currentGame..".lua"))()
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Milan08Studio/ChairWare/main/Games/"..games[game.GameId]..".lua"))();
+(syn and syn.queue_on_teleport or queue_on_teleport or function() end)(game:HttpGet("https://raw.githubusercontent.com/Milan08Studio/ChairWare/main/main.lua"))
